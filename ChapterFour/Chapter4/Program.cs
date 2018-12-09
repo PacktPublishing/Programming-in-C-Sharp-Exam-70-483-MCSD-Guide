f2ce7fffe6e2e69bb81f1e1aef75dca8eeab499d
SrinivasMadhav <srinivasmadhav@gmail.com>
2018-12-09 15:12:16 +1100

    adding Chapter4 samples

---
+++ ChapterFour/Chapter4/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeSamples
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            new Chapter4().OperatorsExamples();
+            //new Chapter4().ConditionalStatementExamples();
+            //new Chapter4().IterationStatementExmples();
+            Console.ReadLine();
+        }
+    }
+}
+
+
