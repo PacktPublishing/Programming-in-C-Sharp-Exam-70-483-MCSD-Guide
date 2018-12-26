690d18d2b4c4e1847c9b8c56a91f1442dc192e2e
srinivasmadhav@gmail.com <srinivasmadhav@gmail.com>
2018-12-26 11:24:09 +1100

    renamed and adding new projects

--- Book70483Samples/Chapter4/Program.cs
+++ Book70483Samples/Chapter4/Program.cs
@@ -1,8 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace CodeSamples
 {
@@ -10,7 +8,7 @@ namespace CodeSamples
     {
         public static void Main(string[] args)
         {
-            new Chapter4().OperatorsExamples();
+            new Chapter4Samples().OperatorsExamples();
             //new Chapter4().ConditionalStatementExamples();
             //new Chapter4().IterationStatementExmples();
             Console.ReadLine();
