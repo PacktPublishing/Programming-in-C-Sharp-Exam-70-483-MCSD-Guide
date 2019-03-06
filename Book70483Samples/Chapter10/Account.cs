397134c31b363647d76aee2cb4e53db15ef8f94b
srinivasmadhav@gmail.com <srinivasmadhav@gmail.com>
2019-03-06 21:01:57 +1100

    Adding chapter 10 related projects

---
+++ Book70483Samples/Chapter10/Account.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Chapter10
+{
+    
+    internal class Account
+    {
+        public string CustomerName { get; set; }
+
+        [Customer]
+        public RatingType Rating { get; set; }
+    }
+
+    public enum RatingType
+    {
+        Gold =1,
+        Silver =2,
+        Bronze=3
+    }
+
+
+    internal class CustomClass1
+    {
+        public int Number1 { get; set; }
+        public int Number2 { get; set; }
+
+        public int Getresult(string action)
+        {
+            int result = 0;
+            switch (action)
+            {
+                case "Add":
+                    result = Number1 + Number2;
+                    Console.WriteLine($"Sum of numbers {Number1} and {Number2} is : {result}");
+                    break;
+
+                case "Subtract":
+                    result = Number1 - Number2;
+                    Console.WriteLine($"Difference of numbers {Number1} and {Number2} is : {result}");
+                    break;
+            }
+            return result;
+        }
+    }
+}
---
+++ Book70483Samples/Chapter10/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chapter10
+{
+    [ChapterInfo("Chapter10", "Madhav")]
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //ChapterInfoAttribute _attribute = (ChapterInfoAttribute)Attribute.GetCustomAttribute(typeof(Program), typeof(ChapterInfoAttribute));
+            //Console.WriteLine($"Chapter Name is: {_attribute.ChapterName} and Chapter Author is: {_attribute.ChapterAuthor}");
+
+            //CallCustomerClass();
+
+            GetResults();
+            // Keep the console window open in debug mode.
+            System.Console.WriteLine("Press any key to exit.");
+            System.Console.ReadKey();
+        }
+
+        private static void CallCustomerClass()
+        {
+            Type _customerType = typeof(Customerclass);
+            object _obj = Assembly.GetExecutingAssembly().CreateInstance(_customerType.FullName);
+            MethodInfo methods = _customerType.GetMethod("DisplayMethod",BindingFlags.Public);
+            methods.Invoke(_obj, new object []{ "Madhav" });
+        }
+
+        public static void GetResults()
+        {
+            Type objType = typeof(CustomClass1);
+            object obj = Activator.CreateInstance(objType);
+            foreach (PropertyInfo prop in objType.GetProperties())
+            {
+                if(prop.Name =="Number1")
+                    prop.SetValue(obj, 100);
+                if (prop.Name == "Number2")
+                    prop.SetValue(obj, 50);
+            }
+
+            MethodInfo mInfo = objType.GetMethod("Getresult");
+            mInfo.Invoke(obj, new string[] { "Add" });
+            mInfo.Invoke(obj, new string[] { "Subtract" });
+        }
+
+    }
+}
